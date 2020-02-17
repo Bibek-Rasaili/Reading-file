75529da217de21ab3d30f669e0ebf5a9ccd8e749
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-17 03:17:02 +0000

    reading file giving output as <buffer 52,65 ... Oa>, perhaps in utf code instead of symbols

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+// const request = require('request');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,7 +9,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
 //careful when using req, and request. - overloading occurs. same for response, res..
+  res.sendFile(__dirname+"/index.html");
+});
 
+app.post("/", function(req, res){
+  console.log("form post is working.");
+
+  fs.readFile(__dirname+"/API.txt", function(error, data){
+    if (error) {
+      console.log("Error! "+error);
+    } else {
+      console.log("No error. now read the file: \n");
+      console.log(data);
+    }
+  });
 });
 
 app.listen(3000, function(){
