2d4d67bca4f7931e24086b25304fb1e76874cab7
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-17 09:01:34 +0000

    Synchronous version of read file

--- app.js
+++ app.js
@@ -5,30 +5,29 @@ const fs = require('fs');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res){
-//careful when using req, and request. - overloading occurs. same for response, res..
-  res.sendFile(__dirname+"/index.html");
+app.get("/", function(req, res) {
+  //careful when using req, and request. - overloading occurs. same for response, res..
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   console.log("form post is working.");
-
-  fs.readFile(__dirname+"/API.txt", function(error, data){
-    if (error) {
-      console.log("Error! "+error);
-    } else {
-      console.log("No error. now read the file: \n");
-      console.log(data.toString()); //This added to "stringify" the output
-
-      res.write("<h1> The file reads: </h1>");
-      res.write("<p>"+data.toString()+"</p>");
-      res.send();
-    }
-  });
+  var data;
+  try {
+    data = fs.readFileSync(__dirname + "/API.txt", 'utf8');
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+  res.write("<h1> The file reads: </h1>");
+  res.write("<p>"+data+"</p>");
+  res.send();
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server listening at port 3000.");
 });
